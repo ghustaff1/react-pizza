93380d78c375ddbdcdac23a0544e8b10a5530fd4
Denys <denismosura@gmail.com>
2023-04-27 13:46:44 +0300

    added pagination, improved pizza-search

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
+import Pagination from '../components/Pagination';
 
 import Services from '../services/services';
 
@@ -15,8 +16,10 @@ const Home = ({ searchValue }) => {
   const [isLoading, setLoading] = React.useState(true);
   const [currentPizzaCategory, setCurrentPizzaCategory] = React.useState(0);
   const [currentPizzaSort, setCurrentPizzaSort] = React.useState('rating');
+  const [currentPage, setCurrentPage]=React.useState(1);
 
-  const _pizzaDataUrl = 'https://6446cd9b0431e885f01c4899.mockapi.io/items';
+  const _pizzaDataUrl = 
+  `https://6446cd9b0431e885f01c4899.mockapi.io/items?page=${currentPage}&limit=4&`;
 
   React.useEffect(() => {
     services.getPizza(_pizzaDataUrl)
@@ -28,22 +31,18 @@ const Home = ({ searchValue }) => {
 
   React.useEffect(() => {
     setLoading(true);
-    services.getPizza(_pizzaDataUrl, currentPizzaCategory, currentPizzaSort)
+    services.getPizza(_pizzaDataUrl, currentPizzaCategory, currentPizzaSort, searchValue)
       .then(json => {
         setItems(json);
         setLoading(false);
       })
-  }, [currentPizzaCategory, currentPizzaSort]);
+  }, [currentPizzaCategory, currentPizzaSort, searchValue, currentPage]);
 
   const skeletons = [...new Array(8)].map((name, i) => <Skeleton key={i} />);
 
-  const pizzas = items.filter(obj => {
-    return obj.title.toLowerCase().includes(searchValue.toLowerCase());
-  }).map((obj) =>
+  const pizzas = items.map((obj) =>
     <PizzaBlock key={obj.id} data={obj} />
   )
-  console.log('searchValue = ', searchValue);
-  console.log('rendered = ', pizzas);
 
   return (
     <div className='container'>
@@ -63,6 +62,8 @@ const Home = ({ searchValue }) => {
           pizzas
         }
       </div>
+      <Pagination
+      onChangePage={setCurrentPage}/>
     </div>
   )
 }
--- src/services/services.js
+++ src/services/services.js
@@ -1,14 +1,18 @@
 class Services {
 
 
-  getPizza = (url, category = 0, sort='rating') => {
-    category ? url += `?category=${category}` : url += '?';
+  getPizza = (url, category = 0, sort='rating', searchValue) => {
+    category ? url += `category=${category}` : url =url;
+
     let order;
     sort.includes('-')?order='desc':order='asc';
     sort=sort.replace('-', '');
-
     url+=`&sortBy=${sort}&order=${order}`;
 
+    searchValue?url+=`&search=${searchValue}`:url=url;
+
+
+    console.log(url)
     return fetch(url).then(data => data.json());
   }
 }
